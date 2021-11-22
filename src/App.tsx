3345a0d8f00ed9f8e99bf337f940960c92d58850
lhk3337 <lhk3337@naver.com>
2021-11-22 22:29:01 +0900

    ErrorBoundary 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Switch, Route, Redirect } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 import { ConnectedRouter } from "connected-react-router";
 import Home from "./pages/Home";
 import Edit from "./pages/Edit";
@@ -8,20 +8,22 @@ import Add from "./pages/Add";
 import Signin from "./pages/Signin";
 import history from "./history";
 import NotFound from "./pages/NotFound";
-
+import { ErrorBoundary } from "react-error-boundary";
+import Error from "./pages/Error";
 function App() {
-  console.log(history);
   return (
-    <ConnectedRouter history={history}>
-      <Switch>
-        <Route exact path="/edit/:id" component={Edit} />
-        <Route exact path="/book/:id" component={Detail} />
-        <Route exact path="/add" component={Add} />
-        <Route exact path="/signin" component={Signin} />
-        <Route exact path="" component={Home} />
-        <Route component={NotFound} />
-      </Switch>
-    </ConnectedRouter>
+    <ErrorBoundary FallbackComponent={Error}>
+      <ConnectedRouter history={history}>
+        <Switch>
+          <Route exact path="/edit/:id" component={Edit} />
+          <Route exact path="/book/:id" component={Detail} />
+          <Route exact path="/add" component={Add} />
+          <Route exact path="/signin" component={Signin} />
+          <Route exact path="/" component={Home} />
+          <Route component={NotFound} />
+        </Switch>
+      </ConnectedRouter>
+    </ErrorBoundary>
   );
 }
 
